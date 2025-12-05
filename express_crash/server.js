84d3517a4637db17fd7b6e898db141cb81cf39f2
Harshit Sonawala <hssonawala@gmail.com>
2025-12-05 15:37:01 +0530

    updated express_crash Dec 5

--- express_crash/server.js
+++ express_crash/server.js
@@ -1,22 +1,38 @@
+// commonjs imports
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 8000;
 
 const app = express(); // main object
 
-// setup static folder to serve
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('/', (req, res) => { // simple route
+// Add a simple route
+// app.get('/', (req, res) => {
 // 	// res.send('Hello From Express Server');
 // 	// res.send('<h3>H3 Header</h3>'); // Can send HTML
 // 	// res.send({ 'Message': 'Can send JSON too!' }); // will stringify it on its own
 
+// Send html files as the route
+// app.get('/', (req, res) => {
 // 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
 
 // app.get('/about', (req, res) => { // adding an additional route
-// 	// res.send('<h3>About Page</h3>');
 // 	res.sendFile(path.join(__dirname, 'public', 'about.html'));
 // });
 
-app.listen(5000, () => console.log('Server is running on port 5000'));
+// Setup static folder to serve
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Serve simple JSON Data
+let posts = [
+	{ id: 1, title: 'Post One' },
+	{ id: 2, title: 'Post Two' },
+	{ id: 3, title: 'Post Three' }
+];
+
+app.get('/api/posts', (req, res) => {
+	res.json(posts);
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
